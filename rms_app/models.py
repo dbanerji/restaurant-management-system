d996d7cb59e5aea1fade2b969c78f91da60db185
dbanerji <deboshruti.banerji95@gmail.com>
2023-12-29 21:06:46 +0000

    Initial model,template

--- rms_app/models.py
+++ rms_app/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
 # Create your models here.
+class Table(models.Model):
+    class Seat(models.IntegerChoices):
+        OneSeater = 1
+        TwoSeater = 2
+        ThreeSeater = 3
+        FourSeater = 4
+        FiveSeater = 5
+        SixSeater = 6
+    class Table_Status(models.TextChoices):
+        Free = "Free"
+        Reserved = "Reserved"
+        Occupied = "Occupied"
+    numberOfSeats = models.IntegerField(
+        max_length=10,
+        choices=Seat,
+        default=1)
+    status = models.CharField(
+        max_length = 10, 
+        choices=Table_Status,
+        default=Table_Status.Free)  
+    
+class Menu_Items(models.Model):
+    class Menu_Section(models.TextChoices):
+        Breakfast = "BreakFast"
+        Lunch = "Lunch"
+        Dinner = "Dinner"
+    section = models.CharField(
+        max_length=30,
+        choices = Menu_Section)
+    name = models.CharField(max_length=65)
+    price = models.FloatField
+
+class Reservations(models.Model):
+    class Reservation_Status(models.TextChoices):
+        Reserved = "Reserved"
+        Canceled = "Canceled"
+    status = models.CharField(max_length=30,choices=Reservation_Status)
+    date = models.DateField()
+    customerName = models.CharField(max_length=100)
+    numberOfPeople = models.IntegerField(max_length=10)
+
+
+
+
--- rms_app/views.py
+++ rms_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request,"rms_app/home.html")
+
+def about(request):
+    return render(request,"rms_app/about.html")
+
+def menu(request):
+    return render(request,"rms_app/menu.html")
+
+def reservations(request):
+    return render(request,"rms_app/reservations.html")
+    
