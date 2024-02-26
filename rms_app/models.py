900f0ae4bb98b150484ad5852cdafedc3832d51a
dbanerji <deboshruti.banerji95@gmail.com>
2024-02-26 17:58:24 +0000

    Logical changes to logic of the program

--- rms_app/models.py
+++ rms_app/models.py
@@ -2,18 +2,8 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+MAX_SEATS = 10
 
-class Table(models.Model):
-    class Seat(models.IntegerChoices):
-        ONE_SEATER = 1
-        TWO_SEATER = 2
-
-    class Table_Status(models.TextChoices):
-        FREE = "Free"
-        RESERVED = "Reserved"
-    number_of_seats = models.IntegerField(Seat.choices)
-    table_status = models.CharField(Table_Status.choices,max_length=20)  
-    
 class Menu_Items(models.Model):
     class Menu_Section(models.TextChoices):
         BREAKFAST = "BreakFast"
@@ -24,19 +14,17 @@ class Menu_Items(models.Model):
     description = models.TextField()
     price = models.FloatField()
 
-class Reservations(models.Model):
-    table = models.ForeignKey(Table, null=True, on_delete=models.CASCADE, related_name='reservations')
-    datetime = models.DateTimeField(default=timezone.now)
-    customer_name = models.CharField(max_length=100)
-    number_of_people = models.IntegerField()
+class Table(models.Model):
+    capacity = models.IntegerField()
+    
+class Guest(models.Model):
+    name = models.CharField(max_length=65)
+    phone_number = models.CharField(max_length=10, unique=True)
 
-class TimeSlots(models.Model):
-    time_start = models.TimeField()
-    time_end = models.TimeField()
-
-class Booking(models.Model):
-    one_seater_available = models.BooleanField()
-    two_seater_available = models.BooleanField()
-
+class Reservation(models.Model):
+    table = models.ForeignKey(Table, null=True, on_delete=models.CASCADE)
+    guest = models.ForeignKey(Guest,null=True,on_delete=models.CASCADE)
+    datetime = models.DateTimeField(default=timezone.now())
+    number_of_guests = models.IntegerField()
 
 
