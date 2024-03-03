3e726bf576f7e1d708cfa76b8001d5604cd3fa0d
dbanerji <deboshruti.banerji95@gmail.com>
2024-03-03 16:54:15 +0000

    Changes made to static files

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -14,8 +14,6 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_DIR = BASE_DIR/"restaurant"/"static"
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -49,6 +47,7 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_DIR = BASE_DIR/"restaurant"/"static"
+STATIC_ROOT = BASE_DIR/"restaurant"
 STATICFILES_DIRS= [
    STATIC_DIR,
 ]
