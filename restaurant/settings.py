f8cb8857e7c47f29ff116d02deaf9e20c85155ea
dbanerji <deboshruti.banerji95@gmail.com>
2024-03-03 17:40:01 +0000

    Changes in requirements

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -11,9 +11,14 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -123,9 +128,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_DIR = BASE_DIR/'restaurant'/'static'
-STATIC_ROOT = BASE_DIR/'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'restaurant', 'static')
 STATICFILES_DIRS= [
    STATIC_DIR,
 ]
