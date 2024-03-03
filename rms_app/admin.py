2ea164c5460d40d7598f4d677c5384a92c7c9677
dbanerji <deboshruti.banerji95@gmail.com>
2024-03-03 19:12:19 +0000

    Integrates psql

--- rms_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from rms_app.models import Menu_Items,Reservation,Table,Guest
-
-# Register your models here.
-admin.site.register(Menu_Items)
-admin.site.register(Reservation)
-admin.site.register(Guest)
-admin.site.register(Table)
