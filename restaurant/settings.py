ccd613ea94d0215c51243ba7a08d7adb268bf24f
dbanerji <deboshruti.banerji95@gmail.com>
2024-03-03 17:29:53 +0000

    Changes in requirements

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'restaurant.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'restaurant', 
         'USER': 'postgres',
         'HOST': 'localhost', 
