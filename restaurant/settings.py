ba27a7f0d882127d6304f84b9a790199b54d660f
dbanerji <deboshruti.banerji95@gmail.com>
2024-03-03 16:58:48 +0000

    Changes in settings

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -126,8 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_DIR = BASE_DIR/"restaurant"/"static"
-STATIC_ROOT = BASE_DIR/"restaurant"
+STATIC_DIR = BASE_DIR/'restaurant'/'static'
+STATIC_ROOT = BASE_DIR/'static/'
 STATICFILES_DIRS= [
    STATIC_DIR,
 ]
